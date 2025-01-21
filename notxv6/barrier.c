90e22af8ef531da2c10ba821a2a8dcc5f3924df8
ubuntu20.04 <https://github.com/R0nAnjou/xv6-lab-2020.git>
2025-01-21 02:58:58 -0800

    uthread3

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -27,9 +27,18 @@ barrier()
 {
   // YOUR CODE HERE
   //
-  // Block until all threads have called barrier() and
+// Block until all threads have called barrier() and
   // then increment bstate.round.
   //
+    pthread_mutex_lock(&bstate.barrier_mutex);
+    if (++bstate.nthread < nthread)
+        pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    else {
+        bstate.nthread = 0;
+        bstate.round++;
+        pthread_cond_broadcast(&bstate.barrier_cond);
+    }
+    pthread_mutex_unlock(&bstate.barrier_mutex);
   
 }
 
