91c5b846f77748d3772cc10ba510b459a2ef8f96
ubuntu20.04 <https://github.com/R0nAnjou/xv6-lab-2020.git>
2025-01-22 22:13:17 -0800

    lock1 and 2

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,12 +1,14 @@
 struct buf {
   int valid;   // has data been read from disk?
   int disk;    // does disk "own" buf?
-  uint dev;
-  uint blockno;
+  uint dev;  //存储设备
+  uint blockno; //块号
   struct sleeplock lock;
-  uint refcnt;
-  struct buf *prev; // LRU cache list
+  uint refcnt; //引用计数，表示当前缓冲区被多少个进程引用
+//  struct buf *prev; // LRU cache list
   struct buf *next;
-  uchar data[BSIZE];
+  uchar data[BSIZE]; //缓冲区数据
+
+  uint lastuse;     //用于跟踪LRU-buf
 };
 
